fcce8d1abf034d9049fef3ca51a42afdb6a10775
Michael Lavelle <michael@lavelle.name>
2020-02-06 21:35:59 +0000

    Adjustments for config of hyperparameters

--- src/main/java/org/ml4j/nn/models/inceptionv4/InceptionV4Factory.java
+++ src/main/java/org/ml4j/nn/models/inceptionv4/InceptionV4Factory.java
@@ -37,6 +37,19 @@ public interface InceptionV4Factory {
 	 * @param context The training or prediction context with which to construct
 	 *                this network. This method may perform additional
 	 *                configurations of this config during execution.
+	 * @param regularisationLambda The regularisation used in the final dense layer.
+	 * @param dropoutKeepProbability The input dropout keep probability used in the final dense layer.       
+	 * @return An inception V4 Network
+	 * @throws IOException In the event that the network cannot be loaded
+	 */
+	SupervisedFeedForwardNeuralNetwork createInceptionV4(FeedForwardNeuralNetworkContext context, float regularisationLambda, float dropoutKeepProbability) throws IOException;
+	
+	/**
+	 * Create a new Inception V4 Network.
+	 * 
+	 * @param context The training or prediction context with which to construct
+	 *                this network. This method may perform additional
+	 *                configurations of this config during execution.
 	 * @return An inception V4 Network
 	 * @throws IOException In the event that the network cannot be loaded
 	 */
@@ -51,10 +64,13 @@ public interface InceptionV4Factory {
 	 * @param outputFeatures The number of output features of the tail.
 	 * @param weights The dense weights
 	 * @param biases The dense biases
+	 * @param regularisationLambda The regularisation used in the final dense layer.
+	 * @param dropoutKeepProbability The input dropout keep probability used in the final dense layer. 
 	 * @return An inception V4 Network
 	 * @throws IOException In the event that the network cannot be loaded
 	 */
-	SupervisedFeedForwardNeuralNetwork createInceptionV4WithCustomTail(FeedForwardNeuralNetworkContext context, int outputFeatures, WeightsMatrix weights, BiasMatrix biases) throws IOException;
+	SupervisedFeedForwardNeuralNetwork createInceptionV4WithCustomTail(FeedForwardNeuralNetworkContext context, int outputFeatures, WeightsMatrix weights, BiasMatrix biases,
+			float regularisationLambda, float dropoutKeepProbability) throws IOException;
 	
 	
 	/**
@@ -66,10 +82,13 @@ public interface InceptionV4Factory {
 	 * @param outputNeuronsCount The number of output neurons of this custom tail
 	 * @param weights The dense weights
 	 * @param biases The dense biases
+	 * @param regularisationLambda The regularisation used in the final dense layer.
+	 * @param dropoutKeepProbability The input dropout keep probability used in the final dense layer. 
 	 * @return An inception V4 Network
 	 * @throws IOException In the event that the network cannot be loaded
 	 */
-	SupervisedFeedForwardNeuralNetwork createInceptionV4Tail(FeedForwardNeuralNetworkContext context, int outputNeuronCount, WeightsMatrix weights, BiasMatrix biases) throws IOException;
+	SupervisedFeedForwardNeuralNetwork createInceptionV4Tail(FeedForwardNeuralNetworkContext context, int outputNeuronCount, WeightsMatrix weights, BiasMatrix biases,
+			float regularisationLambda, float dropoutKeepProbability) throws IOException;
 	
 	
 	/**
